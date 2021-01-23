9fdc425755e60e1b4dbf344d050640ba67a1894e
Mouli <venkatmouli823@gmail.com>
2021-01-23 23:54:03 +0530

    Creating a page for /blog/{slug}

---
+++ src/templates/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout';
+
+const Blog=()=> {
+    return (
+       <Layout>
+           This is the blog Template
+       </Layout>
+    )
+}
+
+export default Blog
