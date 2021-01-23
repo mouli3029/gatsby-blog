61bc1c1330de37ebc3e859a8b27b857bff7432a4
Mouli <venkatmouli823@gmail.com>
2021-01-24 00:13:11 +0530

    Complex accessing html

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import {graphql} from 'gatsby'
 import Layout from '../components/layout';
+// to access context in config-node.js createPage
+export const query = graphql`
+    query(
+        $slug: String
+    ){
+        markdownRemark(
+        fields:{
+            slug:{
+            eq:$slug
+            }
+        }
+        ){
+        frontmatter{
+            title
+            date
+        }
+        html
+        }
+    }
+`
 
-const Blog=()=> {
+const Blog=(props)=> {
     return (
        <Layout>
-           This is the blog Template
+           <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+           <p>{props.data.markdownRemark.frontmatter.date}</p>
+           <div dangerouslySetInnerHTML={{__html : props.data.markdownRemark.html}}>
+
+           </div>
        </Layout>
     )
 }
